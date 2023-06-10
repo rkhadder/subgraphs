75efa7fed502c20b36bdc3e0a3a8e9fba83cd942
Ramsey Khadder <ramseykhadder@hotmail.com>
2023-06-09 17:42:30 -0700

    build schema.graphql

--- subgraphs/savvy/build.sh
+++ subgraphs/savvy/build.sh
@@ -10,5 +10,8 @@ NETWORK=$1
 # Run yarn build:manifest with the specified NETWORK
 yarn build:manifest "$NETWORK"
 
+# Run yarn build:manifest with the specified NETWORK
+yarn build:schema
+
 # Run yarn build:subgraph with the specified subgraph YAML file
 yarn build:subgraph "./setup/manifest/subgraph.$NETWORK.yaml"
